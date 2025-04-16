25e25abd9bf7217c95f69b6465612c530c57eafd
matkurban <3496354336@qq.com>
2025-04-16 19:44:23 +0800

    初始化项目

---
+++ lib/src/widgets/intl_app_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IntlAppBar extends StatelessWidget {
+  const IntlAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text('IntlAppBar'),
+    );
+  }
+}
---
+++ lib/src/widgets/intl_back_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IntlBackButton extends StatelessWidget {
+  const IntlBackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(child: Text('IntlBackButton'));
+  }
+}
